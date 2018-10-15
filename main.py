f569757585826acf3a88feeb3c4c0557f509dfe3
ty <ty.auvil@gmail.com>
2018-10-15 12:17:57 -0600

    refactor to use the hank module

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import json
 import os
-import hank
+from hank import (
+    redis_get,
+    redis_set,
+    url_to_b58_short,
+    write_to_ddb,
+    match_b58,
+    ddb_get_url,
+)
+import logging
 import boto3
+import sys
 from boto3.dynamodb.conditions import Key
-from flask import Flask, abort, flash, request, redirect, render_template
+from flask import Flask, Response, abort, flash, request, redirect, render_template
 from wtforms import Form, TextField, validators
 
 if os.getenv("REDIS_ENABLED"):
@@ -13,13 +22,17 @@ if os.getenv("REDIS_ENABLED"):
     redis_client = redis.Redis(host=redis_host)
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", default="abc123")
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
 
 port = os.getenv("PORT", default=5000)
 flask_url = os.getenv("FLASK_URL", default="http://localhost:5000/")
-dynamodb_endpoint = os.getenv("DYNAMODB_ENDPOINT", default="http://localhost:8000")
-
-dynamodb = boto3.resource("dynamodb", endpoint_url=dynamodb_endpoint)
-table = dynamodb.Table("shortUrl")
 
 
 class ReusableForm(Form):
@@ -30,50 +43,47 @@ class ReusableForm(Form):
 def hello():
     form = ReusableForm(request.form)
 
-    print(form.errors)
+    app.logger.debug(form.errors)
     if request.method == "POST":
         url = request.form["url"]
-        b58_short = hank.url_to_short_b58(url)
+        b58_short = url_to_b58_short(url)
 
         if form.validate():
-            table.put_item(Item={"b58_short": b58_short, "url": url})
-            flash(flask_url + b58_short)
+            write_to_ddb(b58_short, url)
+            flash(flask_url + "/" + b58_short)
         else:
             flash("All the form fields are required. ")
 
     return render_template("submit.html", form=form)
 
 
-@app.route("/v1/url", methods=["POST"])
+@app.route("/url", methods=["POST"])
 def generate_short_url():
-    rtn = {}
     r = request.get_json()
-    rtn["url"] = r["url"]
-    rtn["b58_short"] = hank.url_to_short_b58(r["url"])
-    response = table.put_item(Item={"b58_short": rtn["b58_short"], "url": rtn["url"]})
-    return json.dumps(rtn)
+    url = r["url"]
+    b58_short = url_to_b58_short(url)
+    write_to_ddb(b58_short, url)
+    rtn = {"b58_short": b58_short, "url": url}
+    return Response(json.dumps(rtn), mimetype="application/json")
 
 
 @app.route("/<string:b58_short>")
-def redirect_from_short(b58_short):
-    if not hank.match_b58(b58_short):
+def redirect_from_short_url(b58_short):
+    if not match_b58(b58_short):
         return abort(404)
 
     if os.getenv("REDIS_ENABLED"):
         try:
-            url = redis_client.get(b58_short).decode()
+            url = redis_get(b58_short)
             return redirect(url, code=302)
         except:
             pass
 
-    try:
-        response = table.query(KeyConditionExpression=Key("b58_short").eq(b58_short))
-        url = response["Items"][0]["url"]
-        if os.getenv("REDIS_ENABLED"):
-            redis_client.set(b58_short, url)
-        return redirect(url, code=302)
-    except:
-        pass
+    url = ddb_get_url(b58_short)
+    if os.getenv("REDIS_ENABLED"):
+        redis_set(b58_short, url)
+
+    return redirect(url, code=302)
 
 
 if __name__ == "__main__":
